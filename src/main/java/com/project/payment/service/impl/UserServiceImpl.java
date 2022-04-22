aa15943de520c3ada6f0a3b8af8fd08931ee1724
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-22 05:57:46 -0300

    Criando camada de service de listagem de usuarios

---
+++ src/main/java/com/project/payment/service/impl/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.project.payment.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.payment.model.User;
+import com.project.payment.repository.UserRepository;
+import com.project.payment.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+  
+	@Autowired
+	UserRepository repository;
+
+	@Override
+	public User findById(Long id) {
+ 		return repository.findById(id).orElse(null);
+	}
+
+	@Override
+	public List<User> findAll() {
+ 		return null;
+	}
+}
