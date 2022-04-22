2384efebc00c884a24124e6fc7527f490295337f
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-22 05:49:24 -0300

    Criando repository da entidade User

---
+++ src/main/java/com/project/payment/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.project.payment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.payment.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
