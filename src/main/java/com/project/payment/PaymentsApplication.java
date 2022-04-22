2fd5ad646cd64352f6c0d97903954abff26050d6
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-22 06:42:52 -0300

    Configurando-porta-ambiente

--- src/main/java/com/project/payment/PaymentsApplication.java
+++ src/main/java/com/project/payment/PaymentsApplication.java
@@ -1,13 +1,29 @@
 package com.project.payment;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.project.payment.model.User;
+import com.project.payment.repository.UserRepository;
+
 @SpringBootApplication
-public class PaymentsApplication {
+public class PaymentsApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentsApplication.class, args);
-	}
+    @Autowired
+    private UserRepository userRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentsApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.saveAll(List.of(
+                new User(null, "Mario", "mario@mail.com", "2333", 232.0)
+        ));
+    }
 }
